b831cfc8defd2b555425cc06f32afefdba7b20db
XaVeZ-cmd <thiago.amorim_2@live.com>
2021-07-25 00:57:56 -0300

    corpoSite

--- src/Components/CorpoSite/CorpoSite.js
+++ src/Components/CorpoSite/CorpoSite.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CorpoSite = () => {
   return (
@@ -11,16 +12,16 @@ const CorpoSite = () => {
               <div className="card-body">
                 <h5 className="card-title">Cadastro de Clientes</h5>
                 <p className="card-text">Módulo para cadastro de clientes</p>
-                <a href="/clientes" className="btn btn-primary">Entrar</a>
+                <Link to="/clientes"><button className="btn btn-primary">Entrar</button></Link>
             </div>
           </div></div>
-            <div className="col-sm-6 cardcliente">
+            <div className="col-sm-6 cardprodutos">
               <div className="card">
                   <img className="card-img-top" src="https://www.infovarejo.com.br/novo/wp-content/uploads/2016/12/O-Cadastro-de-Produtos-como-um-diferencial-competitivo-2-1.png" alt="Imagem de capa do card" />
                   <div className="card-body">
                     <h5 className="card-title">Cadastro de Produtos</h5>
                     <p className="card-text">Módulo para cadstro de Produtos</p>
-                    <a href="/produtos" className="btn btn-primary">Entrar</a>
+                    <Link to="/produtos"><button className="btn btn-primary">Entrar</button></Link>
               </div>
             </div>
       </div></div></div>
